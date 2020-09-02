9b2e38619b19af3c4948d64245540c5bce5491e9
Dunker <gfvhhhggffghh@gmail.com>
2020-09-02 12:50:28 +0700

    beta 2

---
+++ js/calculate.js
@@ -0,0 +1,40 @@
+// Const
+const calculateModalElem = document.querySelector('.calculate-modal');
+
+const sqrtCmPrices = { // Коэфы по размерам
+    lowerPrice: 0.5833333333,
+    mediumPrice: 0.4259259259,
+    bigPrice: 0.428541428
+}
+const typesOfSizes = { // Границы размеров
+    lower: 3900 ,
+    medium: 6200, 
+    big: 7000
+}
+const minSize = { // Минимальные размеры
+    x: 30,
+    y: 30
+};
+const maxSize = { // Максимальные размеры
+    x: 250,
+    y: 250
+}
+
+// Calculate func
+function calculate(width, height) {
+    let sqrt = (width * height);
+
+    if (sqrt < typesOfSizes.lower) return Math.floor(sqrtCmPrices.lowerPrice * sqrt);
+    else if (sqrt < typesOfSizes.medium && sqrt > typesOfSizes.lower) return Math.floor(sqrtCmPrices.mediumPrice * sqrt);
+    else if (sqrt > typesOfSizes.medium) return Math.floor(sqrtCmPrices.bigPrice * sqrt); 
+}
+
+// Modal func
+function calculateModal() {
+    calculateModalElem.classList.toggle('hidden');
+    return 0;
+}
+
+
+
+// Event listeners
---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$(".nav__item-link").on("click", function (e) {
+    e.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+
+    if(top > 0 ){
+        $('body,html').animate({scrollTop: top}, 1500);
+    }
+});
