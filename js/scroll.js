1ccbeefce8e5f2dcf31327b9ca6e9abe7fad6e02
Dunker <gfvhhhggffghh@gmail.com>
2020-09-04 16:43:42 +0700

    правки в мобильном менб

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,20 @@
-$(".nav__item-link, .mobile-menu__link").on("click", function (e) {
+$(".nav__item-link").on("click", function (e) {
     e.preventDefault();
     var id  = $(this).attr('href'),
         top = $(id).offset().top;
 
     if(top > 0 ){
+        $('body,html').animate({scrollTop: top}, 1500);
+    }
+});
+
+$(".mobile-menu__link").on("click", function (e) {
+    e.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+
+    if(top > 0 ){
+        $('.wrapper').toggleClass('mobile-menu-active');
         $('body,html').animate({scrollTop: top}, 1500);
     }
 });
